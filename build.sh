b2e609d7f4eefc058620243bdf3d7453b0da1c3d
fengzhaoxin <2673286517@qq.com>
2024-03-24 18:21:39 +0800

    add key

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export CPUS=`grep -c processor /proc/cpuinfo`
 source /opt/fsl-imx-x11/4.1.15-2.0.0/environment-setup-cortexa7hf-neon-poky-linux-gnueabi
-make distclean
+# make distclean
 make imx6ull_elf1_defconfig
 make -j${CPUS}
 rm -rf ./.tmp
